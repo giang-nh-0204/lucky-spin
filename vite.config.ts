f18f3d264bfb7663698b05040a04ffb10d619e30
giangnh <nhgiang.dev@gmail.com>
2025-12-02 14:01:24 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/vong-quay-may-man/',
+  base: process.env.VITE_BASE_URL || '/',
   plugins: [
     vue(),
     vueDevTools(),
